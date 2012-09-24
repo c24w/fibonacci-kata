255d9a309c18dacca24279f759d02df207d089cd
chriswatson <chris.watson@7digital.com>
2012-09-24 17:24:40 +0100

    Initial commit

---
+++ Fibonacci/Fibonacci/Fibonacci.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Fibonacci
+{
+    [TestFixture]
+    class Fibonacci
+    {
+
+        [Test]
+        public void Dumb_test()
+        {
+            Assert.That(true, Is.EqualTo(true));
+        }
+
+    }
+}
